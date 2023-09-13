b07748db016986cdc180c802e680f1076983354d
yw.lee <yw.lee@ivpl.sm.ac.kr>
2023-09-13 22:25:45 +0900

    attn2d 모델 구조 수정

--- models/yolo.py
+++ models/yolo.py
@@ -802,7 +802,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         elif m is Expand:
             c2 = ch[f] // args[0] ** 2
         elif m is ApplyAttention:
-            c2 = 320
+            c2 = args[0]
         else:
             c2 = ch[f]
 
