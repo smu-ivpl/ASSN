9a29cd2a52804920928e34232a7d5c937bff5a22
yw.lee <yw.lee@ivpl.sm.ac.kr>
2023-09-14 22:47:05 +0900

    attention 관련 코드 수정

--- models/yolo.py
+++ models/yolo.py
@@ -801,8 +801,9 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             c2 = ch[f] * args[0] ** 2
         elif m is Expand:
             c2 = ch[f] // args[0] ** 2
-        elif m is ApplyAttention:
-            c2 = args[0]
+        elif m in [ApplyAttention, AttnMap]:
+            if m is ApplyAttention:
+                c2 = args[0]
         else:
             c2 = ch[f]
 
